f3f2f11825a0a27bb9a681acd6d64c42abb2e8cf
Baraka Emmanuel <andiadebayor5@gmail.com>
2026-04-12 15:51:16 +0300

    Add HomeClient component with layout structure

---
+++ app/home-client.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Products from "@/components/Products";
+import Story from "@/components/Story";
+import Gallery from "@/components/Gallery";
+import Testimonials from "@/components/Testimonials";
+import CTA from "@/components/CTA";
+import Contact from "@/components/Contact";
+import WhatsAppButton from "@/components/WhatsAppButton";
+
+export default function HomeClient() {
+  return (
+    <div className="relative">
+      <Navbar />
+      <Hero />
+
+      <div>
+        <Products />
+        <Story />
+        <Gallery />
+        <Testimonials />
+        <CTA />
+        <Contact />
+      </div>
+
+      <WhatsAppButton />
+    </div>
+  );
+}
